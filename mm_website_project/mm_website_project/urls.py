4fb33105c4829cc7ce0fbeec0e50324130bb23b5
Lblk <lblk@tutanota.com>
2021-02-18 12:51:54 +0100

    commit before defining the models

--- mm_website_project/mm_website_project/urls.py
+++ mm_website_project/mm_website_project/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from seances import views
 
 urlpatterns = [
+    url(r'^$', views.home),
+    url(r'^seances/', include('seances.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
---
+++ mm_website_project/seances/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+
+from . import views # import views so we can use them in urls.
+
+urlpatterns = [
+]
