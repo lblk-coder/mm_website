5070d98a8736082e4fdfb5aa04cd5c56591daa73
Lblk <lblk@tutanota.com>
2021-07-14 16:10:05 +0200

    wrote 2 first tests, they work

--- mm_website_project/seances/tests.py
+++ mm_website_project/seances/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Seance, Film, Projection, CarouselSlider
+import datetime
+import pytz
+from io import BytesIO
 
-# Create your tests here.
+# home page test
+class HomePageTestCase(TestCase):
+    def setUp(self):
+        film1 = Film.objects.create(titre="Life of Brian")
+        #  creates a seance for 'tomorrow' based on datetime.date.today() method
+        seance1 = Seance.objects.create(date=datetime.date.today()+datetime.timedelta(days=1))
+        #  creates a projection with timezone infos set on Europe/Paris (= UTC+2)
+        projection1 = Projection.objects.create(
+            film=film1,
+            seance=seance1,
+            heure=datetime.time(13, 00, 00, tzinfo=pytz.timezone('Europe/Paris'))
+        )
+
+    def test_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_objects_exist(self):
+        response = self.client.get(reverse('home'))
+        self.assertIsNotNone(response.context['seances'])
